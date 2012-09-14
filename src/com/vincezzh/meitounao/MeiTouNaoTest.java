378c6edcc7f5d05fbc40b515b9bc56bdaf490fc4
vincezzh@gmail.com <vincezzh@gmail.com>
2012-09-13 21:37:05 -0400

    update MeiTouNao

--- src/com/vincezzh/meitounao/MeiTouNaoTest.java
+++ src/com/vincezzh/meitounao/MeiTouNaoTest.java
@@ -4,9 +4,6 @@ import java.util.List;
 
 import javapns.Push;
 
-import com.notnoop.apns.APNS;
-import com.notnoop.apns.ApnsService;
-
 public class MeiTouNaoTest {
 	public static String myIphoneToken = "962f0ac3e44bdcc567d6bc9f6bf2e6a4685cc5d18ac08316534db645ee8b2def";
 	public static String sukieIphoneToken = "c83f054122c0ecdc3d96e3e0b62fe48089c90d3a057f8cb6a5da9f7a0678b958";
@@ -30,14 +27,14 @@ public class MeiTouNaoTest {
 //		}
 //	}
 	
-	public static void main(String[] args) {
-		ApnsService service =
-		    APNS.newService()
-		    .withCert("/Users/vince/Desktop/aps_developer_identity.p12", "devZHEhan330")
-		    .withSandboxDestination()
-		    .build();
-		String payload = APNS.newPayload().alertBody("Can't be simpler than this!").build();
-		String token = MeiTouNaoTest.myIphoneToken;
-		service.push(token, payload);
-	}
+//	public static void main(String[] args) {
+//		ApnsService service =
+//		    APNS.newService()
+//		    .withCert("/Users/vince/Desktop/aps_developer_identity.p12", "devZHEhan330")
+//		    .withSandboxDestination()
+//		    .build();
+//		String payload = APNS.newPayload().alertBody("Can't be simpler than this!").build();
+//		String token = MeiTouNaoTest.myIphoneToken;
+//		service.push(token, payload);
+//	}
 }
